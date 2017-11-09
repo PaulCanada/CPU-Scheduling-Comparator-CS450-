59429ed9bf632a0c36e5b4120dad4c230b892ced
Paul Canada <paul.canada0@gmail.com>
2017-11-08 19:18:48 -0500

    Initial implementation of main run file
    Added initial implementation of main run file for project.

--- src/cs450assignment1/CS450Assignment1.java
+++ src/cs450assignment1/CS450Assignment1.java
@@ -3,22 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cs450assignment1;
 
 /**
  *
  * @author Paul Canada (https://github.com/PaulCanada)
  */
-public class CS450Assignment1 {
+public class CS450Assignment1
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO code application logic here
-        
-    }
-    
-}
 
+    }
+
+}
