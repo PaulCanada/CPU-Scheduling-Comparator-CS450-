47d9fb9ec1d76d1d2e1354d91dd5bb0fd35a67ee
Paul Canada <paul.canada0@gmail.com>
2017-11-14 13:59:32 -0500

    Initial implementation of files
    Basic files added to repo.

---
+++ src/cs450assignment1/RRQueue.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs450assignment1;
+
+/**
+ *
+ * @author Paul Canada (https://github.com/PaulCanada)
+ */
+public class RRQueue extends ProcessQueue
+{
+    
+}
